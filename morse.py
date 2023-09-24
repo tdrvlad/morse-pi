118831e2ff8e495d7b8819bd980c1eb10ce0df0d
Vlad <tdrvlad@gmail.com>
2023-09-24 19:52:09 +0300

    .

--- morse.py
+++ morse.py
@@ -41,7 +41,7 @@ def get_morse():
 
 def print_current(morse_string, decoded_string):
     print(f"MORSE: {morse_string}\nDECODED: {decoded_string}")
-    display.write(decoded_string[:30], x=50,  y=50, font_size=20)
+    display.write(decoded_string[:10], x=50,  y=50, font_size=40)
 
 
 def time_since_button_released():
