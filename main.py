2195944d12f01b9912fe194ad2b065939fc06653
Vlad <tdrvlad@gmail.com>
2023-09-24 15:24:19 +0300

    Morse

--- main.py
+++ main.py
@@ -1,55 +1,67 @@
 import RPi.GPIO as GPIO
 import time
-from utils import MORSE_CODE_DICT as morse_dict, clear_screen_white, write_centered_text
+from collections import deque
 
-# Setup the GPIO pins
-GPIO.setmode(GPIO.BCM)  # Use BCM mode
 BUTTON_PIN = 27
+DOT_DURATION = 0.3
+DASH_DURATION = 0.7
+DEBOUNCE_TIME = 0.05
+
+WINDOW_SIZE = 5  # Max number of Morse symbols to look back for conversion
+
+# Morse code dictionary
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 
+    'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', 
+    '8': '---..', '9': '----.', '0': '-----', ',': '--..--', '.': '.-.-.-', '?': '..--..', "'": '.----.', 
+    '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', 
+    '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
+}
+
+# Deque will allow us to maintain a moving window buffer of dots/dashes
+buffer = deque(maxlen=WINDOW_SIZE)
+
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-DOT_DURATION = 0.2
-DASH_DURATION = 0.6
-LETTER_GAP = 0.7
+def get_morse_code():
+    pressed_time = 0
+    while GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+        pressed_time += DEBOUNCE_TIME
+        time.sleep(DEBOUNCE_TIME)
+    
+    if pressed_time < DOT_DURATION:
+        return '.'
+    elif pressed_time < DASH_DURATION:
+        return '-'
+    else:
+        return ''
 
+def morse_to_text(morse_code):
+    return MORSE_CODE_DICT.get(morse_code, "")
 
-def get_morse_signal():
-    morse_code = ""
+try:
+    print("Waiting for button press...")
     while True:
         if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-            press_start = time.time()
-            while GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-                time.sleep(0.01)
-            press_duration = time.time() - press_start
-            
-            if press_duration < DOT_DURATION:
-                morse_code += "."
-                write_centered_text(morse_code, 72)
-            elif press_duration < DASH_DURATION:
-                morse_code += "-"
-                write_centered_text(morse_code, 72)
-            else:
-                break
-
-            # Wait to check for space or another morse signal
-            while GPIO.input(BUTTON_PIN) == GPIO.LOW:
-                time.sleep(0.01)
-                if time.time() - press_start > LETTER_GAP:
-                    return morse_dict.get(morse_code, None)
-
-def main():
-    print("Waiting for Morse input...")
-    clear_screen_white()
-    while True:
-        letter = get_morse_signal()
-        if letter:
-            write_centered_text(letter, 72)
-        else:
-            clear_screen_white()
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        clear_screen_white()
-        GPIO.cleanup()
-        print("\nScript terminated.")
+            code = get_morse_code()
+            if code:
+                print(code, end='', flush=True)
+                buffer.append(code)
+                
+                # Convert buffer to text
+                morse_string = ''.join(buffer)
+                text = morse_to_text(morse_string)
+                if text:
+                    print(f" [{text}] ", end='', flush=True)
+                    buffer.clear()  # Clear the buffer if we found a valid letter/symbol
+                    
+            time.sleep(DEBOUNCE_TIME)
+        time.sleep(DEBOUNCE_TIME)
+        
+except KeyboardInterrupt:
+    print("\nScript terminated by user.")
+finally:
+    GPIO.cleanup()
