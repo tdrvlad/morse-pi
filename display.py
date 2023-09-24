8bb039808a515f68e7830d22c829f23ecfeb9b44
Vlad <tdrvlad@gmail.com>
2023-09-24 18:57:38 +0300

    .

--- display.py
+++ display.py
@@ -17,18 +17,25 @@ from PIL import Image,ImageDraw,ImageFont
 logging.basicConfig(level=logging.DEBUG)
 epd = epd2in9_V2.EPD()
 
+import functools
 
-def clear_screen_white():
-    """Clears the screen to white color."""
-    epd.init()
-    epd.Clear(0xFF)
-    epd.sleep()
+def skip_duplicate_calls(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        # Use a unique identifier for the method and its arguments
+        current_call = (func.__name__, args, frozenset(kwargs.items()))
 
-def clear_screen_black():
-    """Clears the screen to black color."""
-    epd.init()
-    epd.Clear(0x00)  # Full black
-    epd.sleep()
+        # If the current call matches the last call, skip it
+        if hasattr(self, '_last_call') and self._last_call == current_call:
+            print(f"Skipped duplicate call: {func.__name__}")
+            return
+
+        # Store the current call as the last call
+        self._last_call = current_call
+
+        return func(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Display:
@@ -37,23 +44,22 @@ class Display:
         last_text = None
         print('Initialized Display')
 
+    @skip_duplicate_calls
     def black_screen(self):
         epd.Clear(0x00)
 
+    @skip_duplicate_calls
     def white_screen(self):
         epd.Clear(0xFF)
 
+    @skip_duplicate_calls
     def write(self, text, font_size, x=20, y=20):
         font = ImageFont.truetype(os.path.join(PICDIR, 'Font.ttc'), font_size)
 
-        # Create an image to draw on
         Himage = Image.new('1', (epd.height, epd.width), 255)
         draw = ImageDraw.Draw(Himage)
-
-        # Get text width and height
         text_width, text_height = draw.textsize(text, font=font)
 
-        # Calculate X, Y position of the text
         if x is None:
             x = (epd.height - text_width) / 2
         if y is None:
@@ -61,97 +67,44 @@ class Display:
 
         draw.text((x, y), text, font=font, fill=0)
         epd.display(epd.getbuffer(Himage))
-        # epd.sleep()
 
+    @skip_duplicate_calls
+    def display_morse_alphabet(self, start_x=5, start_y=3, font_size=14, line_gap=3, column_width=60):
+        font = ImageFont.truetype(os.path.join(PICDIR, 'Font.ttc'), font_size)
+        Himage = Image.new('1', (epd.height, epd.width), 255)
+        draw = ImageDraw.Draw(Himage)
 
-def write_centered_text(text, font_size=12):
-    """Writes the provided text centered on the screen with the given font size."""
-    epd.init()
-    epd.Clear(0xFF)
-    font = ImageFont.truetype(os.path.join(PICDIR, 'Font.ttc'), font_size)
-    
-    # Create an image to draw on
-    Himage = Image.new('1', (epd.height, epd.width), 255)
-    draw = ImageDraw.Draw(Himage)
-    
-    # Get text width and height
-    text_width, text_height = draw.textsize(text, font=font)
+        x, y = start_x, start_y
+        line_height = font_size + line_gap
 
-    # Calculate X, Y position of the text
-    x = (epd.height - text_width) / 2
-    y = (epd.width - text_height) / 2
+        # Loop through Morse code dict and display each letter and its Morse code
+        for char, morse in MORSE_CODE_DICT.items():
+            text = f"{char.upper()} {morse}"
 
-    draw.text((x, y), text, font=font, fill=0)
-    epd.display(epd.getbuffer(Himage))
-    epd.sleep()
+            # If the next line would go off the screen, move to the next column
+            if y + line_height > epd.width - start_y:
+                y = start_y
+                x += column_width
 
+            draw.text((x, y), text, font=font, fill=0)
+            y += line_height
 
-def write_centered_texts(texts, font_size=12):
-    """Writes the provided text centered on the screen with the given font size."""
-    epd.init()
-    epd.Clear(0xFF)
-    font = ImageFont.truetype(os.path.join(PICDIR, 'Font.ttc'), font_size)
+        epd.display(epd.getbuffer(Himage))
 
-    # Create an image to draw on
-    Himage = Image.new('1', (epd.height, epd.width), 255)
-    draw = ImageDraw.Draw(Himage)
+    def sleep(self):
+        epd.sleep()
 
-    # Calculate total height of all lines combined
-    total_text_height = sum([draw.textsize(line, font=font)[1] for line in texts])
-
-    # Calculate starting Y position
-    y = (epd.width - total_text_height) / 2
-
-    for text in texts:
-        # Get text width for the current line
-        text_width, text_height = draw.textsize(text, font=font)
-
-        # Calculate X position of the text
-        x = (epd.height - text_width) / 2
-
-        draw.text((x, y), text, font=font, fill=0)
-
-        # Move the Y position down for the next line
-        y += text_height
-
-    epd.display(epd.getbuffer(Himage))
-    epd.sleep()
-
-
-
-def display_morse_alphabet(start_x=5, start_y=3, font_size=14, line_gap=3, column_width = 60):
-    epd.init()
-    epd.Clear(0xFF)
-    
-    # Set a smaller font size and create a new image
-    font = ImageFont.truetype(os.path.join(PICDIR, 'Font.ttc'), font_size)
-    Himage = Image.new('1', (epd.height, epd.width), 255)
-    draw = ImageDraw.Draw(Himage)
-    
-    x, y = start_x, start_y
-    line_height = font_size + line_gap
-
-    # Loop through Morse code dict and display each letter and its Morse code
-    for char, morse in MORSE_CODE_DICT.items():
-        text = f"{char.upper()} {morse}"
-        
-        # If the next line would go off the screen, move to the next column
-        if y + line_height > epd.width - start_y:
-            y = start_y
-            x += column_width
-        
-        draw.text((x, y), text, font=font, fill=0)
-        y += line_height
-        
-    epd.display(epd.getbuffer(Himage))
-    epd.sleep()
 
 
 if __name__ == '__main__':
     display = Display()
     time.sleep(1)
-    display.write("A", font_size=20)
-    time.sleep(1)
-    display.write("AB", font_size=20)
+    display.write("C", font_size=20)
     time.sleep(1)
     display.write("C", font_size=20)
+    time.sleep(1)
+    display.write("C", font_size=20)
+    time.sleep(1)
+    display.write("C", font_size=20)
+
+
