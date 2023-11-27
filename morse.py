03daacab06ab6e1d8e36484e24a1cb632cb4ec5e
Vlad <tdrvlad@gmail.com>
2023-11-27 21:58:47 +0200

    .

--- morse.py
+++ morse.py
@@ -89,11 +89,11 @@ def main_loop():
     while True:
         decoded = None
         morse_letter = None
-        # if check_reset_button():
-        #     all_morse_words = ""
-        #     morse_word = ''
-        #     decoded_words = ''
-        #     print_current(all_morse_words, decoded_words)
+        if check_reset_button():
+            all_morse_words = ""
+            morse_word = ''
+            decoded_words = ''
+            print_current(all_morse_words, decoded_words)
 
         if GPIO.input(MORSE_INPUT_PIN) == GPIO.HIGH:
             morse_letter = get_morse_unit()
@@ -122,7 +122,6 @@ def main_loop():
 
         if time.time() - last_button_pressed_timestamp > INACTIVITY_THRESHOLD:
             display.display_morse_alphabet()
-        print('Nothing')
         time.sleep(DEBOUNCE_TIME)
 
 try:
